12c40c27debac9e941229fbb7c13e5b1cfd71b42
Aleksey Pavlov <brian.iproject@gmail.com>
2024-12-09 11:01:10 +0300

    Правка стилей бейджа и замена счётчика метрики [ref: no-ref]

--- src/js/main.js
+++ src/js/main.js
@@ -337,7 +337,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
                     if (typeof LoadScripts !== 'undefined') {
                         new LoadScripts([
                             {
-                                html: `<script type="text/javascript">(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym"); ym(11819104, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); <\/script> <noscript><div><img src="https://mc.yandex.ru/watch/11819104" style="position:absolute; left:-9999px;" alt="" \/><\/div><\/noscript>`,
+                                html: `<script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(11819104, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/11819104" style="position:absolute; left:-9999px;" alt="" /></div></noscript>`,
                                 areaSelector: '#loadCounters'
                             },
                             {
